c4ec599477033990d4e861ec102fcf54231d3cda
yiszza <1104573225@qq.com>
2026-01-23 15:30:19 +0800

    Add IconPacks.Generator

---
+++ src/Generator.Test/AvaloniaApp/FeatherIcons/Regular.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaApp.FeatherIcons;
+
+[IconPacks.Generator.AvaloniaGeometryConverter(typeof(IconPacks.Feather.Regular))]
+public static partial class Regular { }
---
+++ src/Generator.Test/WpfApp/FeatherIcons/Regular.cs
@@ -0,0 +1,4 @@
+﻿namespace WpfApp.FeatherIcons;
+
+[IconPacks.Generator.WpfGeometryConverter(typeof(IconPacks.Feather.Regular))]
+public static partial class Regular { }
