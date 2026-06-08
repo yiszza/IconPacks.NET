c6203d8039fe1890683977e4aff185d4e381e5c2
yiszza <1104573225@qq.com>
2026-06-08 23:50:00 +0800

    Update

--- src/IconPacks.Showcase/MainViewModel.cs
+++ src/IconPacks.Showcase/MainViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Input.Platform;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
